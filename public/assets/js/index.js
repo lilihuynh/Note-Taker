30930ad65b4e962c0adc0d86ba8621e804fa72b4
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-26 13:25:38 -0800

    function for getting all notes from the db

---
+++ public/assets/js/index.js
@@ -0,0 +1,16 @@
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
+
+// A function for getting all notes from the db
+const getNotes = () => {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET",
+  });
+};
